7e409b2f34ed3f96e9ca05743dc92239e7f604c8
jihtin <129176693+jihtin@users.noreply.github.com>
2023-06-21 18:02:16 +0530

    navbar

--- Register/Register/settings.py
+++ Register/Register/settings.py
@@ -83,8 +83,7 @@ DATABASES = {
 
 
 # Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-
+# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validato
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
